9664435bc1fd74d348b28dfa3e2435c3635e046d
Justin DeWind <dewind@atomicobject.com>
2011-07-21 07:50:10 -0400

    Initial Commit

---
+++ iOSExample/iOSExampleAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  iOSExampleAppDelegate.h
+//  iOSExample
+//
+//  Created by Justin DeWind on 7/21/11.
+//  Copyright 2011 Atomic Object. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iOSExampleAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
